4d43ef1c9d9c77fd6448116fb79be3ff32f2ea96
Karl <karl.muller@utexas.edu>
2019-04-15 11:41:06 -0500

    added in options for normalization

--- groundXYZ_prediction/upcomingFootComRelative_pull.m
+++ groundXYZ_prediction/upcomingFootComRelative_pull.m
@@ -6,6 +6,45 @@ close all
 feetLocs = [];
 
 velMarkersAll = [];
+
+%% flags
+
+% normalization ( 'leg', 'headRelativeVector', 'none')
+normalizationFlag = 'leg';
+
+% set origin of foot locations ('com','plantfoot')
+originSet = 'plantfoot';
+
+
+
+assert(strcmp(originSet,'plantfoot')||strcmp(originSet,'com'));
+
+switch originSet
+    case 'plantfoot'
+        footOrig=true;
+        outStr_origin = 'plantedFoot';
+    case 'com'
+        footOrig=false;
+        outStr_origin = 'centerOfMass';
+end
+
+
+assert(strcmp(normalizationFlag,'leg')||strcmp(normalizationFlag,'none')||...
+strcmp(normalizationFlag,'headRelativeVector'));
+
+switch normalizationFlag
+    case 'leg'
+        legNorm=1;
+        normalization_str = 'legNormalized';
+    case 'none'
+        legNorm=2;
+        normalization_str = 'not normalized';
+    case 'headRelativeVector'
+        legNorm=3;
+        normalization_str = 'headRelativeVector';
+end
+
+
 %% main iter
 
 
@@ -28,7 +67,7 @@ for subj = 1:3
     end
     
     
-    for cond = 1:endDex
+    for cond = 2:endDex
         disp(['Cond: ' num2str(cond)]);
         switch cond
             case 1
@@ -61,26 +100,46 @@ for subj = 1:3
                 
                 hCenXYZ = thisWalk.hCenXYZ(frameRange,:);
                 
-                feetLoc_this = steps(stepDex+1:min(stepDex+5,size(steps,1)),4:6);
-               
                 comRep =5;
-                feetLoc_this = reshape(feetLoc_this',[1 size(feetLoc_this(:))]);
-                 if size(feetLoc_this,2)~=15
-                    feetLoc_this(1,end+1:15)=nan;
-                end
                 
                 hCenXYZ = repmat(hCenXYZ,[1 comRep]);
                 
-                comRelFeetLocs = feetLoc_this - hCenXYZ;
-                comRelFeetLocs = comRelFeetLocs';            
-                comRelFeetLocs = comRelFeetLocs(:);
-                comRelFeetLocs = reshape(comRelFeetLocs,[3 length(comRelFeetLocs)/3]);
-                comRelFeetLocs = normr(comRelFeetLocs')';
-                comRelFeetLocs = reshape(comRelFeetLocs,[15 size(hCenXYZ,1)])';
-%                 comRelFeetLocs = resh
-                
-                feetLocs = [feetLocs; comRelFeetLocs(2:end,:)];
-                
+                if legNorm==3
+                    feetLoc_this = steps(stepDex+1:min(stepDex+5,size(steps,1)),4:6);
+                    
+                    
+                    feetLoc_this = reshape(feetLoc_this',[1 size(feetLoc_this(:))]);
+                    if size(feetLoc_this,2)~=15
+                        feetLoc_this(1,end+1:15)=nan;
+                    end
+                    
+                    comRelFeetLocs = feetLoc_this - hCenXYZ;
+                    comRelFeetLocs = comRelFeetLocs';
+                    comRelFeetLocs = comRelFeetLocs(:);
+                    comRelFeetLocs = reshape(comRelFeetLocs,[3 length(comRelFeetLocs)/3]);
+                    comRelFeetLocs = normr(comRelFeetLocs')';
+                    comRelFeetLocs = reshape(comRelFeetLocs,[15 size(hCenXYZ,1)])';
+                    
+                    feetLocs = [feetLocs; comRelFeetLocs(2:end,:)];
+                    
+                    
+                elseif legNorm==1
+                    feetLoc_this = steps(stepDex+1:min(stepDex+5,size(steps,1)),[4 6]);
+                    
+                    comRep =5;
+                    feetLoc_this = reshape(feetLoc_this',[1 size(feetLoc_this(:))]);
+                    if size(feetLoc_this,2)~=10
+                        feetLoc_this(1,end+1:10)=nan;
+                    end
+                    
+                    
+                    
+                    stepXZ = steps(stepDex,[4 6]);
+                    stepXZ = repmat(stepXZ,[length(frameRange) comRep]);
+                    planStepRelSteps =  feetLoc_this -stepXZ;
+                    planStepRelSteps = planStepRelSteps/thisWalk.sesh.legLength;
+                    feetLocs = [feetLocs; planStepRelSteps(2:end,:)];
+                end
             end
             
            
@@ -89,4 +148,5 @@ for subj = 1:3
     end
 end
 
-            save('upcomingFeet.mat','feetLocs');
+
+save([outStr_origin '_' normalization_str '_' cond_str '_upcomingFootholds.mat'],'feetLocs');
