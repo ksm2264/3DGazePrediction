8b9e64cc3b9b1f163698f931f2c4088873a7e364
Karl <karl.muller@utexas.edu>
2019-04-15 11:51:24 -0500

    added in options to allpospull.m

--- groundXYZ_prediction/allPosPull.m
+++ groundXYZ_prediction/allPosPull.m
@@ -1,6 +1,42 @@
 clear all
 close all
 
+% normalization ( 'leg', 'headRelativeVector', 'none')
+normalizationFlag = 'leg';
+
+% set origin of foot locations ('com','plantfoot')
+originSet = 'plantfoot';
+
+
+
+assert(strcmp(originSet,'plantfoot')||strcmp(originSet,'com'));
+
+switch originSet
+    case 'plantfoot'
+        footOrig=true;
+        outStr_origin = 'plantedFoot';
+    case 'com'
+        footOrig=false;
+        outStr_origin = 'centerOfMass';
+end
+
+
+assert(strcmp(normalizationFlag,'leg')||strcmp(normalizationFlag,'none'));
+
+switch normalizationFlag
+    case 'leg'
+        legNorm=1;
+        normalization_str = 'legNormalized';
+    case 'none'
+        legNorm=2;
+        normalization_str = 'not normalized';  
+end
+
+
+
+
+
+
 %% stacks
 
 hxAll = [];
@@ -9,8 +45,8 @@ hzAll = [];
 comXYZAll = [];
 markersAll = [];
 gazeXZAll = [];
- gaitCyclePctAll = [];
- velComXYZAll=[];
+gaitCyclePctAll = [];
+velComXYZAll=[];
 velMarkersAll = [];
 %% main iter
 
@@ -22,19 +58,22 @@ for subj = 1:3
         case 1
             sub_str_worldvid = '2018-01-23_JSM';
             sub_str = 'JSM';
+            startDex = 2;
             endDex = 2;
         case 2
             sub_str_worldvid = '2018-01-26_JAC';
             sub_str = 'JAC';
+            startDex = 2;
             endDex = 2;
         case 3
             sub_str_worldvid = '2018-01-31_JAW';
             sub_str = 'JAW';
+            startDex = 2;
             endDex = 2;
     end
     
     
-    for cond = 1:endDex
+    for cond = startDex:endDex
         disp(['Cond: ' num2str(cond)]);
         switch cond
             case 1
@@ -44,7 +83,7 @@ for subj = 1:3
         end
         
         for walk = 1:6
-          
+            
             
             data_path = ['/home/karl/Dropbox/OpticFlowProject/Data/' sub_str_worldvid '/OutputFiles/' ...
                 cond_str '_allWalks.mat'];
@@ -71,11 +110,19 @@ for subj = 1:3
                 % rawCOM
                 comXYZ = thisWalk.comXYZ(frameRange,:);
                 
-             
                 % gaze ground intersection relative to center of mass
-                gazeXZ = mean(cat(3,thisWalk.rGazeGroundIntersection(frameRange,:)-thisWalk.comXYZ(frameRange,:),...
-                    thisWalk.lGazeGroundIntersection(frameRange,:)-thisWalk.comXYZ(frameRange,:)),3);
-                gazeXZ = gazeXZ(:,[1 3]);
+                
+                if footOrig
+                    gazeXZ = mean(cat(3,thisWalk.rGazeGroundIntersection(frameRange,:)-thisWalk.comXYZ(frameRange,:),...
+                        thisWalk.lGazeGroundIntersection(frameRange,:)),3);
+                    gazeXZ = gazeXZ(:,[1 3]);
+                    gazeXZ = gazeXZ - stepXZ;
+                else % com
+                    gazeXZ = mean(cat(3,thisWalk.rGazeGroundIntersection(frameRange,:)-thisWalk.comXYZ(frameRange,:),...
+                        thisWalk.lGazeGroundIntersection(frameRange,:)-thisWalk.comXYZ(frameRange,:)),3);
+                    gazeXZ = gazeXZ(:,[1 3]);
+                end
+                    
                 
                 % percent of the way through a heelstrike to heelstrike
                 gaitCyclePct = linspace(0,1,length(frameRange))';
@@ -85,9 +132,14 @@ for subj = 1:3
                 
                 % markers
                 markers = thisWalk.shadow_fr_mar_dim(frameRange,:,:);
-                comXYZ_sub = permute(repmat(comXYZ,[1 1 30]),[1 3 2]);
-               
-                markers = markers-comXYZ_sub;
+                
+                if footOrig
+                    stepXYZ_sub = permute(repmat(steps(stepDex,[4 5 6]),[1 1 30]),[1 3 2]);
+                    markers = markers-stepXYZ_sub;
+                else
+                    comXYZ_sub = permute(repmat(comXYZ,[1 1 30]),[1 3 2]);
+                    markers = markers-comXYZ_sub;
+                end
                 
                 vel_markers = diff(markers);
                 
@@ -103,26 +155,35 @@ for subj = 1:3
                 hz = thisWalk.headVecZ_fr_xyz(frameRange,:);
                 hz = hz(2:end,:);
                 %                 markers = markers(2:end,:,:);
-              
+                
+                if legNorm ==1
+                    markers = markers/thisWalk.sesh.legLength;
+                    vel_comXYZ = vel_comXYZ/thisWalk.sesh.legLength;
+                    vel_markers = vel_markers/thisWalk.sesh.legLength;
+                    gazeXZ = gazeXZ/thisWalk.sesh.legLength;                   
+                end
+                
+                
+                
                 hxAll = [hxAll;hx];
-hyAll = [hyAll;hy];
-hzAll = [hzAll;hz];
-comXYZAll =[comXYZAll;comXYZ];
-velComXYZAll=[velComXYZAll;vel_comXYZ];
-markersAll = [markersAll;markers];
-velMarkersAll = [velMarkersAll;vel_markers];
-gazeXZAll = [gazeXZAll;gazeXZ];
- gaitCyclePctAll = [gaitCyclePctAll;gaitCyclePct];
+                hyAll = [hyAll;hy];
+                hzAll = [hzAll;hz];
+                comXYZAll =[comXYZAll;comXYZ];
+                velComXYZAll=[velComXYZAll;vel_comXYZ];
+                markersAll = [markersAll;markers];
+                velMarkersAll = [velMarkersAll;vel_markers];
+                gazeXZAll = [gazeXZAll;gazeXZ];
+                gaitCyclePctAll = [gaitCyclePctAll;gaitCyclePct];
                 
             end
             
-           
+            
             
         end
     end
 end
 markerNames = thisWalk.shadowMarkerNames;
 
-            save('bodyfullstack.mat','hyAll','hzAll','hxAll','comXYZAll','markersAll','gazeXZAll',...
-                'gaitCyclePctAll','velComXYZAll','velMarkersAll','markerNames','-v7.3');
+save([outStr_origin '_' normalization_str '_' cond_str '_bodyfullstack.mat'],'hyAll','hzAll','hxAll','comXYZAll','markersAll','gazeXZAll',...
+    'gaitCyclePctAll','velComXYZAll','velMarkersAll','markerNames','-v7.3');
 
